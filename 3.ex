26acca74cb6157de70268e1c76eb54818a8c2ef8
Niklas Klein <niklas.klein@digimondo.de>
2016-12-03 20:39:44 +0100

    use count

--- 3.ex
+++ 3.ex
@@ -15,7 +15,7 @@ defmodule AoC3 do
     |> Stream.chunk(3)
     |> Stream.chunk(3)
     |> Stream.flat_map(&List.zip/1)
-    |> Enum.reduce(0, fn({x,y,z}, acc) -> acc + if ((x + y) > z) and ((x + z) > y) and ((y + z) > x), do: 1, else: 0 end)
+    |> Enum.count(fn {x,y,z} -> ((x + y) > z) and ((x + z) > y) and ((y + z) > x) end)
   end
 
   def run3 do
@@ -27,12 +27,12 @@ defmodule AoC3 do
     |> Stream.flat_map(&List.zip/1)
     |> Stream.map(&Tuple.to_list/1)
     |> Stream.map(&Enum.sort/1)
-    |> Enum.reduce(0, fn([x,y,z], acc) -> acc + if x + y > z, do: 1, else: 0 end)
+    |> Enum.count(fn[x,y,z] -> x + y > z end)
   end
 
   def run4 do
     triangle = fn x,y,z ->
-      if ((x + y) > z) and ((x + z) > y) and ((y + z) > x), do: 1, else: 0
+      ((x + y) > z) and ((x + z) > y) and ((y + z) > x)
     end
     File.stream!("input3.txt")
     |> Stream.flat_map(&String.split/1)
@@ -40,6 +40,6 @@ defmodule AoC3 do
     |> Stream.chunk(3)
     |> Stream.chunk(3)
     |> Stream.flat_map(&apply(:lists, :zipwith3, [triangle|&1]))
-    |> Enum.sum
+    |> Enum.count(fn x -> x end)
   end
 end
